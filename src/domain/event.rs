e25fd58fe238bf261dc75787cba75e91b382a588
Leander Beernaert <lbb-dev@pm.me>
2023-08-07 17:57:09 +0200

    feat: Label Events

--- src/domain/event.rs
+++ src/domain/event.rs
@@ -1,4 +1,4 @@
-use crate::domain::Boolean;
+use crate::domain::{Boolean, Label, LabelId};
 use serde::Deserialize;
 use serde_repr::Deserialize_repr;
 use std::fmt::{Display, Formatter};
@@ -27,11 +27,12 @@ pub struct Event {
     pub event_id: EventId,
     pub more: MoreEvents,
     pub messages: Option<Vec<MessageEvent>>,
+    pub labels: Option<Vec<LabelEvent>>,
 }
 
 #[derive(Debug, Deserialize_repr, Eq, PartialEq, Copy, Clone)]
 #[repr(u8)]
-pub enum MessageAction {
+pub enum EventAction {
     Delete = 0,
     Create = 1,
     Update = 2,
@@ -48,117 +49,13 @@ impl Display for MessageId {
     }
 }
 
-/// Labels API ID. Note that label IDs are used interchangeably between what we would consider
-/// mail labels and mailboxes.
-#[derive(Debug, Deserialize, Eq, PartialEq, Hash, Clone)]
-pub struct LabelID(String);
-
-/// SysLabelID represents system label identifiers that are constant for every account.
-#[derive(Debug, Eq, PartialEq, Hash, Copy, Clone)]
-pub struct SysLabelID(&'static str);
-
-impl PartialEq<LabelID> for SysLabelID {
-    fn eq(&self, other: &LabelID) -> bool {
-        self.0 == other.0
-    }
-}
-
-impl PartialEq<SysLabelID> for LabelID {
-    fn eq(&self, other: &SysLabelID) -> bool {
-        self.0 == other.0
-    }
-}
-
-impl From<SysLabelID> for LabelID {
-    fn from(value: SysLabelID) -> Self {
-        Self(value.0.into())
-    }
-}
-
-impl SysLabelID {
-    pub const INBOX: SysLabelID = SysLabelID("0");
-    pub const ALL_DRAFTS: SysLabelID = SysLabelID("1");
-    pub const ALL_SENT: SysLabelID = SysLabelID("1");
-    pub const TRASH: SysLabelID = SysLabelID("3");
-    pub const SPAM: SysLabelID = SysLabelID("4");
-    pub const ALL_MAIL: SysLabelID = SysLabelID("5");
-    pub const ARCHIVE: SysLabelID = SysLabelID("5");
-    pub const SENT: SysLabelID = SysLabelID("7");
-    pub const DRAFTS: SysLabelID = SysLabelID("8");
-    pub const OUTBOX: SysLabelID = SysLabelID("9");
-    pub const STARRED: SysLabelID = SysLabelID("10");
-    pub const ALL_SCHEDULED: SysLabelID = SysLabelID("12");
-}
-
-impl LabelID {
-    pub fn inbox() -> Self {
-        SysLabelID::INBOX.into()
-    }
-
-    pub fn all_drafts() -> Self {
-        SysLabelID::ALL_DRAFTS.into()
-    }
-
-    pub fn all_sent() -> Self {
-        SysLabelID::ALL_SENT.into()
-    }
-
-    pub fn trash() -> Self {
-        SysLabelID::TRASH.into()
-    }
-
-    pub fn spam() -> Self {
-        SysLabelID::SPAM.into()
-    }
-
-    pub fn all_mail() -> Self {
-        SysLabelID::ALL_MAIL.into()
-    }
-
-    pub fn archive() -> Self {
-        SysLabelID::ARCHIVE.into()
-    }
-
-    pub fn sent() -> Self {
-        SysLabelID::SENT.into()
-    }
-
-    pub fn drafts() -> Self {
-        SysLabelID::DRAFTS.into()
-    }
-
-    pub fn outbox() -> Self {
-        SysLabelID::OUTBOX.into()
-    }
-
-    pub fn starred() -> Self {
-        SysLabelID::STARRED.into()
-    }
-
-    pub fn all_scheduled() -> Self {
-        SysLabelID::ALL_SCHEDULED.into()
-    }
-}
-
-impl Display for LabelID {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        self.0.fmt(f)
-    }
-}
-
-impl Display for SysLabelID {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        self.0.fmt(f)
-    }
-}
-
 /// Event data related to a Message event.
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct MessageEvent {
     #[serde(rename = "ID")]
     pub id: MessageId,
-    pub action: MessageAction,
+    pub action: EventAction,
     pub message: Option<Message>,
 }
 
@@ -169,9 +66,19 @@ pub struct Message {
     #[serde(rename = "ID")]
     pub id: MessageId,
     #[serde(rename = "LabelIDs")]
-    pub labels: Vec<LabelID>,
+    pub labels: Vec<LabelId>,
     pub subject: String,
     pub sender_address: String,
     pub sender_name: Option<String>,
     pub unread: Boolean,
+}
+
+/// Event data related to a Label event
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct LabelEvent {
+    #[serde(rename = "ID")]
+    pub id: LabelId,
+    pub action: EventAction,
+    pub label: Option<Label>,
 }
--- src/domain/labels.rs
+++ src/domain/labels.rs
@@ -3,8 +3,9 @@ use serde::Deserialize;
 use serde_repr::Deserialize_repr;
 use std::fmt::{Display, Formatter};
 
+/// Labels API ID. Note that label IDs are used interchangeably between what we would consider
+/// mail labels and mailboxes.
 #[derive(Debug, Deserialize, Eq, PartialEq, Hash, Clone)]
-/// Id for a proton Label.
 pub struct LabelId(pub String);
 
 impl Display for LabelId {
@@ -16,10 +17,10 @@ impl Display for LabelId {
 #[derive(Debug, Deserialize_repr, Eq, PartialEq, Copy, Clone)]
 #[repr(u8)]
 pub enum LabelType {
-    Label = 0,
-    ContactGroup = 1,
-    Folder = 2,
-    System = 3,
+    Label = 1,
+    ContactGroup = 2,
+    Folder = 3,
+    System = 4,
 }
 
 #[derive(Debug, Deserialize)]
@@ -48,4 +49,97 @@ pub struct Label {
 
 fn default_label_order() -> i32 {
     0
+}
+
+/// SysLabelID represents system label identifiers that are constant for every account.
+#[derive(Debug, Eq, PartialEq, Hash, Copy, Clone)]
+pub struct SysLabelId(&'static str);
+
+impl PartialEq<LabelId> for SysLabelId {
+    fn eq(&self, other: &LabelId) -> bool {
+        self.0 == other.0
+    }
+}
+
+impl PartialEq<SysLabelId> for LabelId {
+    fn eq(&self, other: &SysLabelId) -> bool {
+        self.0 == other.0
+    }
+}
+
+impl From<SysLabelId> for LabelId {
+    fn from(value: SysLabelId) -> Self {
+        Self(value.0.into())
+    }
+}
+
+impl SysLabelId {
+    pub const INBOX: SysLabelId = SysLabelId("0");
+    pub const ALL_DRAFTS: SysLabelId = SysLabelId("1");
+    pub const ALL_SENT: SysLabelId = SysLabelId("1");
+    pub const TRASH: SysLabelId = SysLabelId("3");
+    pub const SPAM: SysLabelId = SysLabelId("4");
+    pub const ALL_MAIL: SysLabelId = SysLabelId("5");
+    pub const ARCHIVE: SysLabelId = SysLabelId("5");
+    pub const SENT: SysLabelId = SysLabelId("7");
+    pub const DRAFTS: SysLabelId = SysLabelId("8");
+    pub const OUTBOX: SysLabelId = SysLabelId("9");
+    pub const STARRED: SysLabelId = SysLabelId("10");
+    pub const ALL_SCHEDULED: SysLabelId = SysLabelId("12");
+}
+
+impl LabelId {
+    pub fn inbox() -> Self {
+        SysLabelId::INBOX.into()
+    }
+
+    pub fn all_drafts() -> Self {
+        SysLabelId::ALL_DRAFTS.into()
+    }
+
+    pub fn all_sent() -> Self {
+        SysLabelId::ALL_SENT.into()
+    }
+
+    pub fn trash() -> Self {
+        SysLabelId::TRASH.into()
+    }
+
+    pub fn spam() -> Self {
+        SysLabelId::SPAM.into()
+    }
+
+    pub fn all_mail() -> Self {
+        SysLabelId::ALL_MAIL.into()
+    }
+
+    pub fn archive() -> Self {
+        SysLabelId::ARCHIVE.into()
+    }
+
+    pub fn sent() -> Self {
+        SysLabelId::SENT.into()
+    }
+
+    pub fn drafts() -> Self {
+        SysLabelId::DRAFTS.into()
+    }
+
+    pub fn outbox() -> Self {
+        SysLabelId::OUTBOX.into()
+    }
+
+    pub fn starred() -> Self {
+        SysLabelId::STARRED.into()
+    }
+
+    pub fn all_scheduled() -> Self {
+        SysLabelId::ALL_SCHEDULED.into()
+    }
+}
+
+impl Display for SysLabelId {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
 }
